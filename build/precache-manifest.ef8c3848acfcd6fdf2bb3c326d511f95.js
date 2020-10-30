7a6f2c0951427dee0297a282ec4e0d299cde7613
01andrewlee <andrewlee1905@gmail.com>
2020-10-30 11:45:19 +0800

    push

---
+++ build/precache-manifest.ef8c3848acfcd6fdf2bb3c326d511f95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96f7ccd90f50f46f053fa35f53659903",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23b42d780f6fccb38d12",
+    "url": "/static/css/main.4025e821.chunk.css"
+  },
+  {
+    "revision": "0ec8a3ad4ae136a1065e",
+    "url": "/static/js/2.84cda420.chunk.js"
+  },
+  {
+    "revision": "f5b692ce6158eff53354301f87a26fd6",
+    "url": "/static/js/2.84cda420.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23b42d780f6fccb38d12",
+    "url": "/static/js/main.837462d4.chunk.js"
+  },
+  {
+    "revision": "e14305b5b1500c6cf806",
+    "url": "/static/js/runtime-main.8a385efa.js"
+  }
+]);
